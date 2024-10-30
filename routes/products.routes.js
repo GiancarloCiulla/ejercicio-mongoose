3ed84dc0f789f94a94c9f6db8e7ada13b2acbdde
unknown <ascis2000@gmail.com>
2024-10-30 10:09:11 +0100

    add/files

---
+++ routes/products.routes.js
@@ -0,0 +1,9 @@
+
+//const fetch = require('node-fetch');
+
+const productsController = require('../controllers/products.controller');
+const router = require('express').Router();
+
+router.get("/:id?", productsController.getProduct);
+
+module.exports = router;
